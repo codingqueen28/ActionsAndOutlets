45ace2039d4b8aeaf1110e1fb799d4b504fc2697
Amanda Wachuku <codewarrior.bubbles@gmail.com>
2022-08-05 15:05:12 -0500

    Initial Commit

---
+++ ActionsAndOutlets/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ActionsAndOutlets
+//
+//  Created by scholar on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var songquestion: UILabel!
+        
+    @IBOutlet weak var textfield: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
+        if let newTitle = textfield.text {
+            songquestion.text = newTitle
+        }
+    }
+    
+}
+
